c744f8d59619d7524db44ddd1e9b405a2bcd3205
oisin odoherty <oisin.odoherty@ucdconnect.ie>
2022-02-21 15:10:20 +0100

    schema generated

--- db/migrate/20220221135521_devise_create_users.rb
+++ db/migrate/20220221135521_devise_create_users.rb
@@ -6,6 +6,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :location
+      t.string :first_name
+      t.string :last_name
+      t.string :user_name
 
       ## Recoverable
       t.string   :reset_password_token
