326e6812d28faed878dc84b141cfbc6439819404
oisin odoherty <oisin.odoherty@ucdconnect.ie>
2022-02-21 16:00:58 +0100

    models/validations/seeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :books, :bookings, dependent: destroy
+  has_many :books
+  has_many :bookings
   has_many :bookings, through: :books
   validates :first_name, :last_name, :user_name, :email, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do
+  user = User.create!(
+    user_name: Faker::Name.name,
+    password: "password",
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: "#{Faker::Name.first_name}#{Faker::Name.last_name}@gmail.com"
+  )
+  10.times do
+    book = Book.new(
+      title: Faker::Book.title,
+      author: Faker::Book.author,
+      suggested_price: 10.0
+    )
+    book.user = user
+    book.save!
+  end
+end
