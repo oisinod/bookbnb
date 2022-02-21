8bd9e272720102a44a99a1246c27c455f5ffeecc
oisin odoherty <oisin.odoherty@ucdconnect.ie>
2022-02-21 17:12:00 +0100

    book route, controller and view created and implemented

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,50 @@
+class BooksController < ApplicationController
+  before_action :set_book, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @books = Book.all
+  end
+
+  def show
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user = current_user
+    if @book.save
+      redirect_to book_path(@book)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @book.update!(book_params)
+      redirect_to book_path(@book)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @book.delete
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author, :suggested_price)
+  end
+
+  def set_book
+    @book = Book.find(params[:id])
+  end
+end
