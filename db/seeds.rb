df244ae6304be31a084a6f45625232243db403de
oisin odoherty <oisin.odoherty@ucdconnect.ie>
2022-02-21 18:03:52 +0100

    seeding files accessing api - but not working yet

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
+require 'open-uri'
+require 'pry-byebug'
+
+url = "https://www.googleapis.com/books/v1/volumes?q=science&key=AIzaSyCGZCM4CcDpgh-aNEYPQcsy-sKcwCEgwIs"
+books = URI.open(url).read
+book = JSON.parse(books)
 
 10.times do
   user = User.create!(
@@ -15,13 +21,21 @@ require 'faker'
     last_name: Faker::Name.last_name,
     email: "#{Faker::Name.first_name}#{Faker::Name.last_name}@gmail.com"
   )
-  10.times do
-    book = Book.new(
-      title: Faker::Book.title,
-      author: Faker::Book.author,
-      suggested_price: 10.0
+  book.first(10) do |result|
+    p result
+    binding.pry
+    new_book = Book.new(
+      title: result["items"]["volumeInfo"]["title"],
+      author: result["items"]["volumeInfo"]["authors"][0],
+      suggested_price: 10.0,
+      photo_url: result["items"]["volumeInfo"]["imageLinks"]["thumbnail"]
     )
-    book.user = user
-    book.save!
+    puts new_book
+    new_book.user = user
+    new_book.save!
   end
 end
+
+# first_book = book["items"].first
+# puts first_book["volumeInfo"]["title"]
+# puts first_book["volumeInfo"]["imageLinks"]["thumbnail"]
