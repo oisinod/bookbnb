455fef80b354e516b294cbfe9e4e373d6285a8de
oisin odoherty <oisin.odoherty@ucdconnect.ie>
2022-02-22 14:40:44 +0100

    photo added to book form and also to the book show page with cloudinary

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,5 @@
 class Book < ApplicationRecord
+  has_one_attached :photo
   belongs_to :user
   has_many :bookings, dependent: :destroy
   validates :title, :author, :suggested_price, presence: true
