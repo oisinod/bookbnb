93edf24541a20d1ff747aa434fe953978c99bfb8
oisin odoherty <oisin.odoherty@ucdconnect.ie>
2022-02-22 11:59:05 +0100

    booking feature implemented

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,6 +6,7 @@ class BooksController < ApplicationController
   end
 
   def show
+    @bookings = @book.bookings
   end
 
   def new
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,33 +8,42 @@
 require 'faker'
 require 'open-uri'
 
-url = "https://www.googleapis.com/books/v1/volumes?q=science&key=AIzaSyCGZCM4CcDpgh-aNEYPQcsy-sKcwCEgwIs"
-books = URI.open(url).read
-book = JSON.parse(books)
-10.times do
-  user = User.create!(
-    user_name: Faker::Name.name,
-    password: "password",
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
-    email: "#{Faker::Name.first_name}#{Faker::Name.last_name}@gmail.com"
-  )
-  book["items"].first(10).each do |result|
-    author = result["volumeInfo"]["authors"]
-    author = "" if author.nil?
-    photo_url = result["volumeInfo"]["imageLinks"]
-    photo_url = "" if result["volumeInfo"]["imageLinks"].nil?
-    new_book = Book.new(
-      title: result["volumeInfo"]["title"],
-      author: author[0],
-      suggested_price: 10.0,
-      photo_url: photo_url["thumbnail"]
-    )
-    new_book.user = user
-    new_book.save
-  end
-end
+# url = "https://www.googleapis.com/books/v1/volumes?q=science&key=AIzaSyCGZCM4CcDpgh-aNEYPQcsy-sKcwCEgwIs"
+# books = URI.open(url).read
+# book = JSON.parse(books)
+# 10.times do
+#   user = User.create!(
+#     user_name: Faker::Name.name,
+#     password: "password",
+#     first_name: Faker::Name.first_name,
+#     last_name: Faker::Name.last_name,
+#     email: "#{Faker::Name.first_name}#{Faker::Name.last_name}@gmail.com"
+#   )
+#   book["items"].first(10).each do |result|
+#     author = result["volumeInfo"]["authors"]
+#     author = "" if author.nil?
+#     photo_url = result["volumeInfo"]["imageLinks"]
+#     photo_url = "" if result["volumeInfo"]["imageLinks"].nil?
+#     new_book = Book.new(
+#       title: result["volumeInfo"]["title"],
+#       author: author[0],
+#       suggested_price: 10.0,
+#       photo_url: photo_url["thumbnail"]
+#     )
+#     new_book.user = user
+#     new_book.save
+#   end
+# end
 
-# first_book = book["items"].first
-# puts first_book["volumeInfo"]["title"]
-# puts first_book["volumeInfo"]["imageLinks"]["thumbnail"]
+### SEED FOR BOOKINGS
+5.times do
+  booking = Booking.new(
+    price: 10,
+    start_date: Date.new(2022,2,22),
+    end_date: Date.new(2022,2,24),
+    status: "Booked"
+  )
+  booking.book = Book.first
+  booking.user = User.first
+  booking.save
+  end
