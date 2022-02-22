c076d13ea50b2eabc87252c7ad6430e52e58cdaa
oisin odoherty <oisin.odoherty@ucdconnect.ie>
2022-02-22 18:17:11 +0100

    js button added and form created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @books = @user.books
+    @review = Review.new
     # TO DO: Add pundit to here as it will otherwise show the bookings of all users?
     @outgoing_bookings = @user.my_bookings
   end
