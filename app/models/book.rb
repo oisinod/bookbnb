c18592eecfb1c2c1e14e66c351b3f131df44f229
Marc Hannemann <hannemann-marc@web.de>
2022-02-24 15:08:45 +0100

    Add PG_search

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,4 +4,14 @@ class Book < ApplicationRecord
   has_many :bookings, dependent: :destroy
   validates :title, :author, :suggested_price, presence: true
   validates :suggested_price, numericality: { only_float: true }
+
+  include PgSearch::Model
+  pg_search_scope :global_search,
+    against: [ :title, :author, :description ],
+    associated_against: {
+      user: [ :user_name ]
+    },
+    using: {
+      tsearch: { prefix: true }
+    }
 end
