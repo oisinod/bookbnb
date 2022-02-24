3611c2dcab727255b96a7df3fbe995ba0b16d3fc
oisin odoherty <oisin.odoherty@ucdconnect.ie>
2022-02-25 00:50:35 +0100

    fixing reviews

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,5 +9,28 @@ class UsersController < ApplicationController
     @review = Review.new
     # TO DO: Add pundit to here as it will otherwise show the bookings of all users?
     @outgoing_bookings = @user.my_bookings
+    @total_reviews = total_reviews(@user)
+    @reviews = my_reviews(@user)
+    @user = User.find(params[:id])
+  end
+
+  def total_reviews(user)
+    total = 0
+    user.books.select do |b|
+       b.bookings.each do |r|
+       total += 1 if r.review
+       end
+     end
+     total
+     raise
+
+     #TO DO FIX THIS
+  end
+
+  def my_reviews(user)
+    reviews = user.books.map do |book|
+      book.bookings.where(status: "confirmed")
+    end
+    reviews.select {|booking| booking.length > 0}
   end
 end
