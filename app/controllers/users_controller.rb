768e44a02aca9b35121c23448d8bbc83589d2168
oisin odoherty <oisin.odoherty@ucdconnect.ie>
2022-02-25 15:37:51 +0100

    glfknsdfpoijs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,14 +5,21 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @books = @user.books #array of the users books that you're looking at
+    @books = @user.books
+
+    #array of the users books that you're looking at
     #iterate through each book to see its bookings
-      # IF the booking has a review attached to it
+    # IF the booking has a review attached to it
     @users_reviews = []
     @books.each do |book|
       book.bookings.each do |booking|
         booking.review ? @users_reviews.push(booking.review) : next
       end
     end
+    @total_score = 0
+    @ratings = @users_reviews.each do |review|
+      @total_score += review.rating
+    end
+    @average_rating = @total_score/@users_reviews.count
   end
 end
